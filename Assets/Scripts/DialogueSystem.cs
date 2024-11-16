3ced29a1fdebdf03dba9f47017c6d10d77e207a9
Wiktoria Przybylak <s28750@pjwstk.edu.pl>
2024-11-16 11:19:40 +0100

    TexMeshPro + simple dialogue system

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEditor;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+
+    private void Start()
+    {
+        dialogueText.text = string.Empty;
+        StartDialogue();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (dialogueText.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
