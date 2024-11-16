34d3a13fd1861997ce23a153a396db38d2673760
Aleksander Cyran <s27730@pjwstk.edu.pl>
2024-11-16 10:47:16 +0100

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float movSpeed;
+    
+    private float speedX,speedY;
+    
+    
+    private Rigidbody2D _rb;
+    
+    
+    
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        _rb.velocity = new Vector2(speedX, speedY);
+    }
+}
