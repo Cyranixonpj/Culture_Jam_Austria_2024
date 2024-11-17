da457591b4e3daf17428100858ce162d48dae7f5
Wiktoria Przybylak <s28750@pjwstk.edu.pl>
2024-11-17 10:47:41 +0100

    first level

--- Assets/Scripts/Interact/ObjectInteract.cs
+++ Assets/Scripts/Interact/ObjectInteract.cs
@@ -8,6 +8,7 @@ public class ObjectInteract : MonoBehaviour, IInteractable
     [SerializeField] private WordInfo requriedWord;
     [SerializeField] private GameObject popup;
     private bool _inRange;
+    public bool goodWord = false;
 
     void Start()
     {
@@ -28,11 +29,16 @@ public class ObjectInteract : MonoBehaviour, IInteractable
     public void CheckIfCorrectWordSelected()
     {
         if (WordHolder.instance._lastSelectedWord == requriedWord)
+        {
             Debug.Log("Zniszczone");
+            goodWord = true;
+        }
         else
         {
             var textObject = Instantiate(popup, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
             textObject.GetComponentInChildren<TextMeshProUGUI>().text = "WRONG WORD";
+            Debug.Log("Zniszczone");
+            goodWord = false;
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
