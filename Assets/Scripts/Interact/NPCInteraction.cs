54a70c7e341f320a8db874e5f63d88db7617943e
Aleksander Sznerch <aleksander.sznerch@gmail.com>
2024-11-25 12:56:39 +0100

    Highlight
    Implemented highlight effect.
    Using the NPCInteract and ObjectInteract Scripts
    Other small tweaks

--- Assets/Scripts/Interact/NPCInteraction.cs
+++ Assets/Scripts/Interact/NPCInteraction.cs
@@ -5,7 +5,24 @@ using UnityEngine;
 public class NPCInteraction : MonoBehaviour, IInteractable
 {
     [SerializeField] private NPCInfo _NPCInfo;
+    [SerializeField] private Material HighlightMaterial; // Material for highlight effect
     private List<NPCInteraction> _npcsInRange = new List<NPCInteraction>();
+    private SpriteRenderer _spriteRenderer;
+    private Material _originalMaterial;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        if (_spriteRenderer != null)
+        {
+            _originalMaterial = _spriteRenderer.material; // Save the original material
+            _spriteRenderer.material = new Material(_spriteRenderer.material); // Ensure unique material instance
+        }
+        else
+        {
+            Debug.LogError("SpriteRenderer is missing! Ensure this GameObject has a SpriteRenderer component.");
+        }
+    }
 
     void Update()
     {
@@ -24,6 +41,7 @@ public class NPCInteraction : MonoBehaviour, IInteractable
         if (other.gameObject.CompareTag("Player"))
         {
             _npcsInRange.Add(this);
+            EnableHighlight(); // Enable highlight when the player enters the range
         }
     }
 
@@ -32,6 +50,7 @@ public class NPCInteraction : MonoBehaviour, IInteractable
         if (other.gameObject.CompareTag("Player"))
         {
             _npcsInRange.Remove(this);
+            DisableHighlight(); // Disable highlight when the player leaves the range
         }
     }
 
@@ -55,10 +74,33 @@ public class NPCInteraction : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-       
         string[] dialog = _NPCInfo.lines;
         DialogueSystem.Instance.StartDialogue(dialog);
         if (_NPCInfo.GivenWord != null) 
             WordHolder.instance.AddWord(_NPCInfo.GivenWord);
+    }
+
+    private void EnableHighlight()
+    {
+        if (_spriteRenderer != null && HighlightMaterial != null)
+        {
+            Debug.Log("Highlight enabled.");
+            _spriteRenderer.material = HighlightMaterial; // Switch to highlight material
+            _spriteRenderer.material.SetFloat("_Highlight", 1); // Set highlight parameter
+        }
+        else
+        {
+            Debug.LogError("Highlight material or SpriteRenderer is not set!");
+        }
+    }
+
+    private void DisableHighlight()
+    {
+        if (_spriteRenderer != null && _originalMaterial != null)
+        {
+            Debug.Log("Highlight disabled.");
+            _spriteRenderer.material.SetFloat("_Highlight", 0); // Reset highlight parameter
+            _spriteRenderer.material = _originalMaterial; // Switch back to original material
+        }
     }
 }
